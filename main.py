95a4e9866066a1eefb0d781a8716c83b2f10025e
MChrisp <maxchristianpotass@gmail.com>
2022-04-23 16:17:23 +0200

    Created window with top and sidebar with settings.py as parameters.

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import settings
+root = Tk()
+# Override settings of window
+
+root.configure(bg='grey')  # Configure can do way more than bg (Background color)
+root.geometry(f'{settings.root_width}x{settings.root_height}')
+root.title("Minesweeper - beta")
+# root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='white',
+    width=settings.top_frame_width,
+    height=settings.top_frame_height,
+)
+top_frame.place(x=0, y=0)
+
+side_frame = Frame(
+    root,
+    bg='black',
+    width=settings.side_frame_width,
+    height=settings.side_frame_height
+)
+
+side_frame.place(x=0, y=settings.top_frame_height)
+
+# Run Window
+root.mainloop()  # Window stays open until closed
---
+++ settings.py
@@ -0,0 +1,11 @@
+from math import *
+
+root_width = 1000
+root_height = 500
+
+top_frame_height = floor(0.2 * root_height)
+top_frame_width = root_width
+
+side_frame_height = root_height - top_frame_height
+side_frame_width = floor(root_width * 0.2)
+
