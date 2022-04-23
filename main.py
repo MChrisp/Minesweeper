ee118d16e6c23976c3c06b3a98d46c9ed0988b2a
MChrisp <m.potass@pm.me>
2022-04-23 17:31:08 +0200

    Signing works now.
    Added cell.py and Cell Class with btn create button function.
    
    Added some buttons in main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from tkinter import *
 import settings
 import utils
+from cell import Cell
 
 root = Tk()
 # Override settings of window
 
-root.configure(bg='grey')  # Configure can do way more than bg (Background color)
+root.configure(bg='black')  # Configure can do way more than bg (Background color)
 root.geometry(f'{settings.root_width}x{settings.root_height}')
 root.title("Minesweeper - beta")
 # root.resizable(False, False)
@@ -16,24 +17,48 @@ top_frame = Frame(
     width=settings.top_frame_width,
     height=settings.top_frame_height,
 )
-top_frame.place(x=settings.top_frame_x, y=settings.top_frame_y)
+
+top_frame.place(
+    x=settings.top_frame_x,
+    y=settings.top_frame_y
+)
 
 left_frame = Frame(
     root,
-    bg='black',
+    bg='white',
     width=settings.left_frame_width,
     height=settings.left_frame_height
 )
 
-left_frame.place(x=settings.left_frame_x, y=settings.left_frame_y)
+left_frame.place(
+    x=settings.left_frame_x,
+    y=settings.left_frame_y
+)
 
 center_frame = Frame(
-    bg='green',
+    bg='grey',
     width=settings.center_frame_width,
     height=settings.center_frame_height
 )
 
-center_frame.place(x=settings.center_frame_x, y=settings.center_frame_y)
+center_frame.place(
+    x=settings.center_frame_x,
+    y=settings.center_frame_y
+)
+
+c1 = Cell()
+c1.create_btn_object(center_frame)
+c1.cell_btn_object.place(
+    x=0, y=0
+)
+
+c2 = Cell()
+c2.create_btn_object(center_frame)
+2.cell_btn_object.place(
+    x=0,
+    y=0
+)
 
 # Run Window
 root.mainloop()  # Window stays open until closed
+
